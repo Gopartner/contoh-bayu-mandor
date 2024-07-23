3b3e8b416cfe454c3d15332a80e65b6253154c1e
Gopartner <smartfrenpaketansatu@gmail.com>
2024-07-24 02:27:05 +0700

    membuat file enviromw t variable

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,16 +2,18 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
+// Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDQyR1nbgZEmZaKgU-7WT0pA_BbWWlZUSA",
-  authDomain: "contoh-judi-online.firebaseapp.com",
-  databaseURL: "https://contoh-judi-online-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "contoh-judi-online",
-  storageBucket: "contoh-judi-online.appspot.com",
-  messagingSenderId: "210939919406",
-  appId: "1:210939919406:web:97f2196cf488e0f56633bf"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
